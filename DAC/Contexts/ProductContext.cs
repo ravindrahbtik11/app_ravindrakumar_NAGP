557ad8bed21dc898a147ffc7f1abf31639a0410f
Ravindra Kumar <ravindra.kumar@nagarro.com>
2023-02-27 13:35:01 +0530

    First commit of User service

---
+++ DAC/Contexts/ProductContext.cs
@@ -0,0 +1,15 @@
+﻿using eCommerce.ProductService.DAC.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace eCommerce.ProductService.DAC.Contexts
+{
+    public class ProductContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public ProductContext(DbContextOptions<ProductContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
